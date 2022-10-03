b3e36b375bbf00bdc4cdf91fa9969abd966f596d
igolubevJ <igolubevapp@gmail.com>
2022-10-04 02:03:47 +0300

    switch/case

--- bin/dart_basic.dart
+++ bin/dart_basic.dart
@@ -1,3 +1,15 @@
 void main(List<String> arguments) {
- 
+  // Switch / Case
+  String grade = "B";
+
+  switch (grade) {
+    case "A":
+      print("Excelent grade of A");
+      break;
+    case "B":
+      print("Very Good");
+      break;
+    default:
+      print("Failed");
+  }
 }
