f654876fe1b973019eec1b4b40caa3e94e4d7c3f
igolubevJ <igolubevapp@gmail.com>
2022-10-04 02:21:58 +0300

    while loop

--- bin/dart_basic.dart
+++ bin/dart_basic.dart
@@ -1,10 +1,7 @@
 void main(List<String> arguments) {
   int counter = 0;
-  do {
-    counter++;
-    if (counter == 4) {
-      continue;
-    }
+  while (counter < 10) {
     print("Hello Dart! counter = $counter");
-  } while (counter < 10);
+    counter++;
+  }
 }
