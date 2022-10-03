8dae43cbae87fdb56bfc9d0887fbaf1c687bce7c
igolubevJ <igolubevapp@gmail.com>
2022-10-04 02:05:13 +0300

    clear

--- bin/dart_basic.dart
+++ bin/dart_basic.dart
@@ -1,15 +1,1 @@
-void main(List<String> arguments) {
-  // Switch / Case
-  String grade = "B";
-
-  switch (grade) {
-    case "A":
-      print("Excelent grade of A");
-      break;
-    case "B":
-      print("Very Good");
-      break;
-    default:
-      print("Failed");
-  }
-}
+void main(List<String> arguments) {}
