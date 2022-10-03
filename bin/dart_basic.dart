082d5e0bccee7abb2ad5c1b7a050bea3cdeae8e3
igolubevJ <igolubevapp@gmail.com>
2022-10-04 01:09:08 +0300

    create primitive data types

--- bin/dart_basic.dart
+++ bin/dart_basic.dart
@@ -1,12 +1,41 @@
-class Person {
-  final name = "Илья";
-  static const age = 30;
-}
-
 void main(List<String> arguments) {
-  const myAge = 30;
+  // Number (int, double)
+  int myAge = 30;
+  print(myAge.runtimeType);
 
-  final myName = "Илья";
+  double myWeight = 80.6;
+  print(myWeight.runtimeType);
 
-  print("$myName $myAge");
+  // Boolean
+  bool isPerson = true;
+  print(isPerson.runtimeType);
+
+  // String (UTF-16)
+  String myName = "Илья";
+  String mySurname = 'Голубев';
+
+  print(myName.runtimeType);
+  print(mySurname.runtimeType);
+
+  String question = 'What\'s you name?';
+  print(question);
+
+  String fio = '''
+  Голубев
+  Илья
+  Николаевич
+  ''';
+  print(fio);
+
+  String personInfo = "Name ${myName.toUpperCase()} Weight: $myWeight";
+  print(personInfo);
+
+  // Runes (UTF-32)
+  Runes hello = Runes('\u041F\u0440\u0438\u0432\u0435\u0442');
+  print(hello);
+  print(String.fromCharCodes(hello));
+
+  // Symbol
+  Symbol s = #f;
+  print(s);
 }
