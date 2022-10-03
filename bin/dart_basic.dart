e59e5d39f7c592d8a677b02134b80447a731acea
igolubevJ <igolubevapp@gmail.com>
2022-10-04 00:19:32 +0300

    Create variable

--- bin/dart_basic.dart
+++ bin/dart_basic.dart
@@ -1,5 +1,7 @@
-import 'package:dart_basic/dart_basic.dart' as dart_basic;
-
 void main(List<String> arguments) {
-  print('Hello world: ${dart_basic.calculate()}!');
+  int myAge = 30;
+
+  var myName = "Илья";
+
+  print("$myName $myAge");
 }
