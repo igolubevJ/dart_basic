d51bfb285576d5bad9cd2447a2f22221659fc577
igolubev <igolubevapp@gmail.com>
2022-10-04 09:03:59 +0300

    named params

--- bin/dart_basic.dart
+++ bin/dart_basic.dart
@@ -1,6 +1,7 @@
 void main(List<String> arguments) {
   getPerson(name: "John", age: 30);
   getPerson(age: 27, name: "Jane");
+  getPerson();
 }
 
 void getPerson({String name = '<empty>', int age = 0}) {
