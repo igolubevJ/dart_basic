60b7e74f488a3677e3d9de1928431fc9e2e60395
igolubev <igolubevapp@gmail.com>
2022-10-04 08:31:06 +0300

    Insert field to map

--- bin/dart_basic.dart
+++ bin/dart_basic.dart
@@ -1,4 +1,16 @@
 void main(List<String> arguments) {
   var map = {"key": "value", "key2": "value2"};
   print(map);
+
+  Map map1 = {1: "value-1", 2: "value-2"};
+  print(map1);
+
+  Map<String, String> traffickLight = Map();
+
+  // Add field
+  traffickLight['red'] = 'stop';
+  traffickLight['yellow'] = 'go?';
+  traffickLight['green'] = 'go';
+
+  print(traffickLight);
 }
