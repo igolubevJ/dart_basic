ad3da978d6134077b155824c88ed3acedac47874
igolubev <igolubevapp@gmail.com>
2022-10-04 11:04:01 +0300

    clear

--- bin/dart_basic.dart
+++ bin/dart_basic.dart
@@ -1,29 +1,6 @@
-void main(List<String> arguments) {
-  Car car = Car("Ford", color: "light");
-  car.start();
-
-  Car bmw = Car.named();
-  bmw.start();
-
-  Car bmwWithColor = Car.fromColor("orange");
-  bmwWithColor.start();
-}
+void main(List<String> arguments) {}
 
 class Car {
   String carName = "";
-  var color = "";
-
-  Car.named() : this("BMW", color: "green");
-
-  Car.fromColor(var color) : this("BMW", color: color);
-
-  Car(this.carName, {this.color = "blue"});
-
-  void start() {
-    print("The $color $carName has started");
-  }
-
-  void stop() {
-    print("This $color $carName has stopped");
-  }
+  double percentWeight = 0.0;
 }
