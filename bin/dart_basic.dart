696d851880dabdeb2098677004ec4075cc594597
igolubev <igolubevapp@gmail.com>
2022-10-04 11:15:20 +0300

    clear

--- bin/dart_basic.dart
+++ bin/dart_basic.dart
@@ -1,18 +1,8 @@
-void main(List<String> arguments) {
-  var car = Car(name: "BMW", per: 23.6);
-  car.infoCar();
-}
+void main(List<String> arguments) {}
 
 class Car {
   String carName = "";
   double percentWay = 0.0;
-
-  Car({String name = "", double per = 0.0})
-      : carName = name,
-        percentWay = per.roundToDouble() {
-    print("Other work");
-    percentWay = 100;
-  }
 
   void infoCar() {
     print("The $carName drove $percentWay of the way");
