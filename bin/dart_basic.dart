b7d351878564c196e4254223feda68a544ae8777
igolubev <igolubevapp@gmail.com>
2022-10-04 09:07:38 +0300

    store func in variable

--- bin/dart_basic.dart
+++ bin/dart_basic.dart
@@ -1,1 +1,13 @@
-void main(List<String> arguments) {}
+void main(List<String> arguments) {
+  var person = getName;
+
+  person();
+}
+
+void getName() {
+  print('Jane');
+}
+
+void getAge() {
+  print('Dou');
+}
