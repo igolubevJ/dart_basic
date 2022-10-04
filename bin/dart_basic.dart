cff0d9aaa915bff4b7b5f7234d7e0cc6d4db2ed9
igolubev <igolubevapp@gmail.com>
2022-10-04 10:13:14 +0300

    print stack trace

--- bin/dart_basic.dart
+++ bin/dart_basic.dart
@@ -10,12 +10,13 @@ void main(List<String> arguments) {
   // }
 
   try {
-    List list = [double.parse("3.14"), 2, 3];
+    List list = [double.parse("3,14"), 2, 3];
     print(list[5]);
   } on RangeError {
     print("Out of bound of list");
-  } catch (e) {
+  } catch (e, s) {
     print("The Exception: $e");
+    print("Stack: $s");
   } finally {
     print("This is finally and is always executed");
   }
