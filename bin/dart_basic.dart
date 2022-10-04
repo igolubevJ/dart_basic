648bc5a6198445719cb7ceb82b640b8106d24e9b
igolubevJ <igolubevapp@gmail.com>
2022-10-04 03:08:12 +0300

    create list

--- bin/dart_basic.dart
+++ bin/dart_basic.dart
@@ -1,1 +1,15 @@
-void main(List<String> arguments) {}
+void main(List<String> arguments) {
+  List<int> list1 = [1, 2, 3];
+
+  var list2 = List<String>.filled(10, '<empty>');
+
+  var list3 = [];
+
+  var list4 = [1, 'string', true];
+
+  var list5 = [
+    [1, 2, 3],
+    ['hello'],
+    [true, false]
+  ];
+}
