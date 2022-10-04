65d702ef3cce75d73ad02f52f8eb2eebbc7e8a15
igolubev <igolubevapp@gmail.com>
2022-10-04 11:54:34 +0300

    control static variable from static method

--- bin/dart_basic.dart
+++ bin/dart_basic.dart
@@ -2,18 +2,26 @@ void main(List<String> arguments) {
   var car = Car("BMW", "red");
   car.checkMove();
 
-  Car.wheels = 3;
+  Car.setWheels(3);
   car.checkMove();
 }
 
 class Car {
   String carName = "Ford";
   String color = "black";
-  static int wheels = 4;
+  static int _wheels = 4;
 
   Car(this.carName, this.color) {
     var t = time(500, 80);
     print("Time: $t");
+  }
+
+  static void setWheels(int value) {
+    if (value == 4) {
+      _wheels = value;
+    } else {
+      print("Please, correct value (4)");
+    }
   }
 
   static double time(int dist, double speed) {
@@ -21,7 +29,7 @@ class Car {
   }
 
   void checkMove() {
-    if (wheels < 4) {
+    if (_wheels < 4) {
       print("This $carName is broken");
     } else {
       print("The $color $carName has started");
