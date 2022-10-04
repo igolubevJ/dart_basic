cbd183c1a21ebd4d43b509cbb7c395568835bd21
igolubevJ <igolubevapp@gmail.com>
2022-10-04 04:29:15 +0300

    insert element to set

--- bin/dart_basic.dart
+++ bin/dart_basic.dart
@@ -1,16 +1,12 @@
 void main(List<String> arguments) {
-  var set = {1, 2, 3};
-  print(set);
-
-  Set<int> set1 = {1, 2, 3};
-  print(set1);
-
-  var set2 = <int>{1, 2, 3};
-  print(set2);
-
-  Set<int> set3 = Set.from([1, 2, 3]);
-  print(set3);
-
   Set<int> numberSet = Set();
+  print(numberSet);
+
+  // Insert elements
+  numberSet.add(10);
+  numberSet.add(11);
+  numberSet.add(12);
+  numberSet.add(10);
+
   print(numberSet);
 }
