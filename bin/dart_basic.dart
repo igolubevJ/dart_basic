874b2fd9e77d33caf80a2f18517fac84fc42975b
igolubev <igolubevapp@gmail.com>
2022-10-04 09:33:32 +0300

    create anonymous function

--- bin/dart_basic.dart
+++ bin/dart_basic.dart
@@ -1,1 +1,14 @@
-void main(List<String> arguments) {}
+void main(List<String> arguments) {
+  // ignore: prefer_function_declarations_over_variables
+  Function addToNumbers = (int a, int b) {
+    int sum = a + b;
+    print('[anonymous] $a + $b = $sum');
+  };
+
+  addToNumbers(10, 5);
+}
+
+void addNumbers(int a, int b) {
+  var sum = a + b;
+  print("[function] $a + $b = $sum");
+}
