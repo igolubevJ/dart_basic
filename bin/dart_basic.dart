13a62a198d2990b614fe41d693a3e11612f627c9
igolubev <igolubevapp@gmail.com>
2022-10-04 10:30:33 +0300

    Add some field props and methods

--- bin/dart_basic.dart
+++ bin/dart_basic.dart
@@ -1,7 +1,18 @@
 void main(List<String> arguments) {
   Car car = Car();
+  car.carName = "Ford";
+  car.color = "black";
+
+  car.start();
 
   print(car);
 }
 
-class Car {}
+class Car {
+  String carName = "";
+  var color = "";
+
+  void start() {
+    print("The $color $carName has started");
+  }
+}
