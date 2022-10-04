facb208d4e48617fcdf296a4974b5a460ea826eb
igolubev <igolubevapp@gmail.com>
2022-10-04 11:09:09 +0300

    initializer constructor

--- bin/dart_basic.dart
+++ bin/dart_basic.dart
@@ -1,6 +1,19 @@
-void main(List<String> arguments) {}
+void main(List<String> arguments) {
+  var car = Car(name: "BMW", per: 23.6);
+  car.infoCar();
+}
 
 class Car {
   String carName = "";
-  double percentWeight = 0.0;
+  double percentWay = 0.0;
+
+  Car({String name = "", double per = 0.0})
+      : carName = name,
+        percentWay = per {
+    print("Other work");
+  }
+
+  void infoCar() {
+    print("The $carName drove $percentWay of the way");
+  }
 }
