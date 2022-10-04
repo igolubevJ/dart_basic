84cda8d7a407fde06ba794c912b77e3bf27074d8
igolubev <igolubevapp@gmail.com>
2022-10-04 09:59:07 +0300

    function clojure

--- bin/dart_basic.dart
+++ bin/dart_basic.dart
@@ -1,1 +1,14 @@
-void main(List<String> arguments) {}
+void main(List<String> arguments) {
+  Function add = addNumber(10);
+
+  int result = add(20);
+  print(result);
+}
+
+Function addNumber(int x) {
+  int sum(int y) {
+    return x + y;
+  }
+
+  return sum;
+}
