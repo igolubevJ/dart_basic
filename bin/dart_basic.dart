2de7f21a4ae0f5b59d399a4057ec2f8e1cc81c80
igolubev <igolubevapp@gmail.com>
2022-10-04 09:04:37 +0300

    clear

--- bin/dart_basic.dart
+++ bin/dart_basic.dart
@@ -1,9 +1,1 @@
-void main(List<String> arguments) {
-  getPerson(name: "John", age: 30);
-  getPerson(age: 27, name: "Jane");
-  getPerson();
-}
-
-void getPerson({String name = '<empty>', int age = 0}) {
-  print("name: $name, age: $age");
-}
+void main(List<String> arguments) {}
