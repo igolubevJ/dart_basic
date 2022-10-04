befd7fd3f640533ca77740be00a4165bb6cbfbe6
igolubev <igolubevapp@gmail.com>
2022-10-04 09:55:56 +0300

    variable scope

--- bin/dart_basic.dart
+++ bin/dart_basic.dart
@@ -1,1 +1,32 @@
-void main(List<String> arguments) {}
+String topLevelVariable = "Top Level Variable";
+
+void main(List<String> arguments) {
+  String variableInsideMain = "Variable Inside Main";
+
+  void myFunction() {
+    String variableInsideMyFunction = "Variable Inside My Function";
+
+    print("Inside myFunction");
+
+    void nestedFunction() {
+      String variableInsideNestedFunction = "Variable Inside Nested Function";
+      print("Inside nestedFunction");
+
+      print("[nestedFunction] $topLevelVariable");
+      print("[nestedFunction] $variableInsideMain");
+      print("[nestedFunction] $variableInsideMyFunction");
+      print("[nestedFunction] $variableInsideNestedFunction");
+    }
+
+    nestedFunction();
+
+    print("[myFunction] $topLevelVariable");
+    print("[myFunction] $variableInsideMain");
+    print("[myFunction] $variableInsideMyFunction");
+  }
+
+  myFunction();
+
+  print("[main] $topLevelVariable");
+  print("[main] $variableInsideMain");
+}
