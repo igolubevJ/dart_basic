059299758792be943ef1f55c6278d2a649065048
igolubev <igolubevapp@gmail.com>
2022-10-04 11:30:55 +0300

    custom getters and setters

--- bin/dart_basic.dart
+++ bin/dart_basic.dart
@@ -3,13 +3,27 @@ void main(List<String> arguments) {
 
   car.carName = "Ford"; // use default Setters
   print(car.carName); // use default Getters
+
+  car.percentWay = 90;
+  print(car.percentWay);
+
+  car.percentWay = 200;
+  print(car.percentWay);
 }
 
 class Car {
   String carName = "";
-  double percentWay = 0.0;
+  double _percentWay = 0.0;
+
+  set percentWay(double per) {
+    _percentWay = per > 100 || per < 0 ? 0 : per;
+  }
+
+  double get percentWay {
+    return _percentWay;
+  }
 
   void infoCar() {
-    print("The $carName drove $percentWay of the way");
+    print("The $carName drove $_percentWay of the way");
   }
 }
