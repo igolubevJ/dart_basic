8835132872158de547c1c987941dca6ab8acd71c
igolubev <igolubevapp@gmail.com>
2022-10-04 09:38:59 +0300

    use anonymous func in operator

--- bin/dart_basic.dart
+++ bin/dart_basic.dart
@@ -4,6 +4,13 @@ void main(List<String> arguments) {
       (int a, int b) => print("[anonymous short] $a + $b = ${a + b}");
 
   addToNumbers(2, 5);
+
+  int result = operationNumbers(10, 2, (int a, int b) => a - b);
+  print('result is equel => $result');
+}
+
+int operationNumbers(int a, int b, Function operation) {
+  return operation(a, b);
 }
 
 void addNumbers(int a, int b) {
