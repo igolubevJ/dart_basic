2ca6c67d317244c3c08ee6304959f34805f60359
igolubev <igolubevapp@gmail.com>
2022-10-04 10:37:48 +0300

    Create constructor

--- bin/dart_basic.dart
+++ bin/dart_basic.dart
@@ -1,10 +1,16 @@
 void main(List<String> arguments) {
-  Car car = Car();
+  Car car = Car("Ford", "black");
+  car.start();
 }
 
 class Car {
   String carName = "";
   var color = "";
+
+  Car(String carName, var color) {
+    this.carName = carName;
+    this.color = color;
+  }
 
   void start() {
     print("The $color $carName has started");
