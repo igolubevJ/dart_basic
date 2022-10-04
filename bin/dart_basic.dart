c4704e0c343ce01d3b25972033a83061278b69a4
igolubev <igolubevapp@gmail.com>
2022-10-04 10:08:33 +0300

    try/catch

--- bin/dart_basic.dart
+++ bin/dart_basic.dart
@@ -1,1 +1,18 @@
-void main(List<String> arguments) {}
+void main(List<String> arguments) {
+  // Exception
+  // Error
+
+  // try {
+  //   List list = [double.parse("3,14"), 2, 3];
+  //   print(list);
+  // } catch (e) {
+  //   print("The Exception: $e");
+  // }
+
+  try {
+    List list = [double.parse("3.14"), 2, 3];
+    print(list[5]);
+  } catch (e) {
+    print("The Exception: $e");
+  }
+}
