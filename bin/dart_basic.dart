82f19224a5269a6e8feabeddc4ee6029e5623c23
igolubev <igolubevapp@gmail.com>
2022-10-04 09:00:19 +0300

    optional params

--- bin/dart_basic.dart
+++ bin/dart_basic.dart
@@ -1,7 +1,8 @@
 void main(List<String> arguments) {
   getPerson("John", 30);
+  getPerson("Jane");
 }
 
-void getPerson(String name, int age) {
+void getPerson(String name, [int age = 0]) {
   print("name: $name, age: $age");
 }
