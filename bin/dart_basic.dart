86ce1b94da67e7c15cdaf93df25c762b00f3d9b8
igolubevJ <igolubevapp@gmail.com>
2022-10-04 03:29:17 +0300

    we can create dynamic list other way

--- bin/dart_basic.dart
+++ bin/dart_basic.dart
@@ -1,5 +1,5 @@
 void main(List<String> arguments) {
-  List<dynamic> numberList = List.filled(5, 0);
+  List numberList = List.filled(5, 0);
   print(numberList);
 
   numberList[0] = 22;
@@ -14,11 +14,11 @@ void main(List<String> arguments) {
   numberList[0] = 25;
   print('After update first element, numberList is equal $numberList');
 
-  for (dynamic i = 0; i < numberList.length; i++) {
+  for (var i = 0; i < numberList.length; i++) {
     print('$i -> ${numberList[i]}');
   }
 
-  for (dynamic element in numberList) {
+  for (var element in numberList) {
     print('element = $element');
   }
 
