9e0d1ccc2723dd80a2a8adfa12b6f94d7933b0f2
igolubev <igolubevapp@gmail.com>
2022-10-04 08:53:18 +0300

    create func

--- bin/dart_basic.dart
+++ bin/dart_basic.dart
@@ -1,1 +1,24 @@
-void main(List<String> arguments) {}
+void main(List<String> arguments) {
+  sayHello();
+
+  String name = getName();
+  print(name);
+
+  String age = getAge(30);
+  print(age);
+}
+
+// [тип] имя_функции(параметры) {
+//    тело функции
+// }
+void sayHello() {
+  print('Hello Dart');
+}
+
+String getName() {
+  return 'Илья';
+}
+
+String getAge(int age) {
+  return 'You age $age';
+}
