f0cd4cb6f818cb86d67abca7fbcf7740f072144a
igolubev <igolubevapp@gmail.com>
2022-10-04 10:02:59 +0300

    clear

--- bin/dart_basic.dart
+++ bin/dart_basic.dart
@@ -1,17 +1,1 @@
-void main(List<String> arguments) {
-  Function add = addNumber(10);
-
-  int result = add(20);
-  print(result);
-
-  int result2 = addNumber(1)(20);
-  print(result2);
-}
-
-Function addNumber(int x) {
-  int sum(int y) {
-    return x + y;
-  }
-
-  return sum;
-}
+void main(List<String> arguments) {}
