84fbd9cb8fa9ed8b14e548fc8c2ff22c72a54c36
igolubev <igolubevapp@gmail.com>
2022-10-04 11:38:14 +0300

    create final props

--- bin/dart_basic.dart
+++ bin/dart_basic.dart
@@ -1,11 +1,16 @@
 void main(List<String> arguments) {
-  var car = Car();
+  var car = Car("BMW", 40.0);
+  car.move();
+
+  print("Car have ${car.wheels} wheels");
 }
 
 class Car {
   String carName = "Ford";
   double percentWay = 0.0;
-  int wheels = 4;
+  final int wheels = 4;
+
+  Car(this.carName, this.percentWay);
 
   void move() {
     print("The $carName has started. Percent way: $percentWay");
