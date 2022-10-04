f2a4eaccff3473590f94dc785f28e201892db470
igolubev <igolubevapp@gmail.com>
2022-10-04 11:34:37 +0300

    clear

--- bin/dart_basic.dart
+++ bin/dart_basic.dart
@@ -1,29 +1,13 @@
 void main(List<String> arguments) {
   var car = Car();
-
-  car.carName = "Ford"; // use default Setters
-  print(car.carName); // use default Getters
-
-  car.percentWay = 90;
-  print(car.percentWay);
-
-  car.percentWay = 200;
-  print(car.percentWay);
 }
 
 class Car {
-  String carName = "";
-  double _percentWay = 0.0;
+  String carName = "Ford";
+  double percentWay = 0.0;
+  int wheels = 4;
 
-  set percentWay(double per) {
-    _percentWay = per > 100 || per < 0 ? 0 : per;
-  }
-
-  double get percentWay {
-    return _percentWay;
-  }
-
-  void infoCar() {
-    print("The $carName drove $_percentWay of the way");
+  void move() {
+    print("The $carName has started. Percent way: $percentWay");
   }
 }
